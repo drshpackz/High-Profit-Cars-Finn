71ad0f30a50e3a62c36cbe8391f3e143384f3f9e
drshpackz <55964535+drshpackz@users.noreply.github.com>
2023-04-10 05:41:24 +0200

    Update LinkToCarsInfo.py

--- LinkToCarsInfo.py
+++ LinkToCarsInfo.py
@@ -8,24 +8,36 @@ def get_html(url):
     response = requests.get(url)
     return response.text
 
+def is_ad_removed(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    removed_text = 'Inaktiv'
+    removed_element = soup.find(lambda tag: tag.name == 'div' and tag.get('class') == ['panel', 'u-pv0'] and removed_text in tag.text)
+    return removed_element is not None
 
 def extract_data(html, url):
     soup = BeautifulSoup(html, 'html.parser')
 
     # Extract car brand and model
-    nav = soup.find('nav', {'class': 'finn-breadcrumbs-component'})
-    car_brand = nav.find_all('a')[-2].text.strip()
-    car_model = nav.find_all('a')[-1].text.strip()
+    try:
+        nav = soup.find('nav', {'class': 'finn-breadcrumbs-component'})
+        car_brand = nav.find_all('a')[-2].text.strip()
+        car_model = nav.find_all('a')[-1].text.strip()
+    except AttributeError:
+        car_brand = 'Unknown'
+        car_model = 'Unknown'
 
     # Extract panel data
-    section = soup.find('section', {'class': 'panel panel--bleed summary-icons'})
-    data_divs = section.find_all('div', {'class': 'media__body'})
+    try:
+        section = soup.find('section', {'class': 'panel panel--bleed summary-icons'})
+        data_divs = section.find_all('div', {'class': 'media__body'})
 
-    data = {}
-    for div in data_divs:
-        key = div.find('div').text.strip()
-        value = div.find('div', {'class': 'u-strong'}).text.strip()
-        data[key] = value
+        data = {}
+        for div in data_divs:
+            key = div.find('div').text.strip()
+            value = div.find('div', {'class': 'u-strong'}).text.strip()
+            data[key] = value.replace('\xa0', ' ')
+    except AttributeError:
+        data = {}
 
     # Extract other fields
     fields = [
@@ -35,24 +47,33 @@ def extract_data(html, url):
     ]
 
     for field in fields:
-        field_div = soup.find('dt', string=field)
-        if field_div:
-            value = field_div.find_next_sibling('dd').text.strip()
-            value = ' '.join(value.split())
-            data[field] = value
+        try:
+            field_div = soup.find('dt', string=field)
+            if field_div:
+                value = field_div.find_next_sibling('dd').text.strip()
+                value = ' '.join(value.split())
+                data[field] = value.replace('\xa0', ' ')
+        except AttributeError:
+            data[field] = 'Unknown'
 
     # Calculate total price
-    omreg = int(re.sub(r'\D', '', data['Omregistrering']))
-    pris_eks = int(re.sub(r'\D', '', data['Pris eks omreg']))
-    total_sum = omreg + pris_eks
+    try:
+        omreg = int(re.sub(r'\D', '', data['Omregistrering'])) if data['Omregistrering'] else 0
+        pris_eks = int(re.sub(r'\D', '', data['Pris eks omreg'])) if data['Pris eks omreg'] else 0
+        total_sum = omreg + pris_eks
+    except (KeyError, ValueError):
+        total_sum = 'Unknown'
 
     # Skip Sylindervolum for electric cars
     if data.get('Drivstoff') == 'Elektrisitet':
         data['Sylindervolum'] = 'N/A'
 
     # Extract CarID
-    car_id_div = soup.find('span', {'data-adid': True})
-    car_id = car_id_div['data-adid']
+    try:
+        car_id_div = soup.find('span', {'data-adid': True})
+        car_id = car_id_div['data-adid']
+    except AttributeError:
+        car_id = 'Unknown'
 
     # Combine all data
     final_data = {
@@ -68,6 +89,7 @@ def extract_data(html, url):
     final_data.pop('Pris eks omreg', None)
 
     return final_data
+
 
 
 def save_to_csv(data, filename):
@@ -96,17 +118,19 @@ def main():
     if urls:
         # Write the CSV header using the first URL's data
         html = get_html(next(iter(urls)))
-        data = extract_data(html, next(iter(urls)))
-        write_csv_header(filename, data.keys())
+        if not is_ad_removed(html):
+            data = extract_data(html, next(iter(urls)))
+            write_csv_header(filename, data.keys())
 
         # Process all unique URLs
         for url in urls:
             html = get_html(url)
+            if is_ad_removed(html):
+                print(f"Ad removed from {url}")
+                continue
             data = extract_data(html, url)
             save_to_csv(data, filename)
             print(f"Data saved from {url}")
 
-
 if __name__ == '__main__':
     main()
-
